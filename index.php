706bc715a11c99463eb5cfc2e137a1971a651ced
slava102938.slava <slava102938@inbox.lv>
2018-04-22 11:48:43 +0300

    Modified general page, add new font, image and sound

--- index.php
+++ index.php
@@ -3,31 +3,24 @@ session_start();
 
 	require "php/connectBD.php";
 	
-
-	// Вход на страницу
-	if(isset($_POST['login'])) {
+	if(isset($_POST['login']))
+	{
 		$login = $_POST['login'];
 		$inquiryBD = "SELECT * FROM users WHERE login='$login'";
 		$resultArray = mysqli_query(hostopen(), $inquiryBD);
 		foreach($resultArray as $result) {
 		}
-		
+	}
+	// Вход на страницу
 		if(isset($_SESSION['id']) or (isset($_COOKIE['login']) and testCookie($_COOKIE['login'], $_COOKIE['cookie']) == true) or (isset($_POST['login']) and testData($_POST['login'],$_POST['email'],$_POST['password']) == true)) {
-			if($result['baned'] == 0) {
-				if(!isset($_SESSION['id'])) {
-					$_SESSION['id'] = $result['id'];
-					$_SESSION['cookkod'] = genCode();
-				}
-				if(isset($_POST['savecookie'])) {
-					$_SESSION['savecookie'] = $_POST['savecookie'];
-				}
-				header("refresh:0.1;url='profile.php'");
+			if(!isset($_SESSION['id'])) {
+				$_SESSION['id'] = $result['id'];
+				$_SESSION['cookkod'] = genCode();
 			}
-			else {
-				?>
-				<script>alert('Вы были забанены')</script>
-				<?php
+			if(isset($_POST['savecookie'])) {
+				$_SESSION['savecookie'] = $_POST['savecookie'];
 			}
+			header("refresh:0.1;url='profile.php'");
 		}
 		else { 
 			if(isset($_POST['login']) and testData($_POST['login'],$_POST['email'],$_POST['password']) != true) {
@@ -36,7 +29,6 @@ session_start();
 				<?php
 			}
 		}
-	}
 
 	// Функция для проверки пользователя в базе данных
 	function testData($login,$email,$password) {
@@ -96,38 +88,35 @@ session_start();
 	<meta charset="utf-8">
 		<title>Главная</title>
 		<link rel="stylesheet" type="text/css" href="css/indexCss.css">
-		<style>
-			[type="submit"]{
-				color: #9FEE00;
-			}
-			[type="reset"]{
-				color: #FB000D;
-			}
-		</style>
 		<meta name="robots" content="noindex, nofollow">
 		<noscript><meta http-equiv="refresh" content="0.1;noscript.html"></noscript>
 	</head>
 	<body>
-	<span onclick="showNote()">!!!Можно нажать!!!</span>
-	<div class="note" style="display: none;">Все фоновые картинки на сайте были скачаны с бесплатного источника!</div>
-
-	<div id="form">
-		<form action="" method="POST"> 
-			<fieldset>
-			<legend>Форма авторизации</legend>
-			<input type="text" name="login" placeholder="Введите логин" required minlength="4" maxlength="15" value="<?php if(isset($_POST['login'])) echo $_POST['login'] ?>"><br>
-			<input type="email" name="email" placeholder="Введите эмайл" required value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>"><br>
-			<input id="pass" type="password" name="password" placeholder="Введите пароль" required minlength="6" maxlength="20"><br>
-			<label><input type="checkbox" onclick="showPass()">Показать пароль</label><br>
-			<label><input type="checkbox" name="savecookie" <?php if(isset($_POST['savecookie'])) { ?> checked <?php } ?>>Запомнить меня</label><br>
-			<input type="submit" name="" value="Войти">
-			<input type="reset" onclick="notShowPass()"><br>
-			</fieldset>
-		</form>
-		<a href="registration.php">Регистрация</a>
-		<a href="recovery.php">Забыл пароль</a><br>
-		<span class="email">При любых ошибках писать на эл. почту: <a href="mailto:slava102938@inbox.lv">slava102938@inbox.lv</a><span>
-	</div>
+	<main>
+		<div id="form">
+			<form action="" method="POST"> 
+				<fieldset>
+					<legend>Форма авторизации</legend>
+					<input type="text" name="login" placeholder="Введите логин" required minlength="4" maxlength="15" value="<?php if(isset($_POST['login'])) echo $_POST['login'] ?>"><br>
+					<input type="email" name="email" placeholder="Введите эмайл" required value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>"><br>
+					<input id="pass" type="password" name="password" placeholder="Введите пароль" required minlength="6" maxlength="20"><br>
+					<div class="labelBlock">
+						<label><input type="checkbox" onclick="showPass()"><span class="infoLabel">Показать пароль</span></label>
+						<label><input type="checkbox" name="savecookie" onclick="saveUser()"><span class="infoLabel">Запомнить меня</span></label>
+					</div>
+					<input type="submit" name="" value="Войти">
+					<div class="submitBlock submitBlockTop"></div>
+					<div class="submitBlock submitBlockBottom"></div>
+					<div class="submitBlock submitBlockRight"></div>
+					<div class="submitBlock submitBlockLeft"></div>
+				</fieldset>
+			</form><br><br><br>
+			<hr><br>
+			<a href="registration.php">Регистрация</a><br>
+			<a href="recovery.php">Забыл пароль</a><br>
+			<a href="help.php" onclick="return false;">Тех.поддержка</a>
+		</div>
+	</main>
 	<script asyns src="js/indexJs.js"></script>
 	</body>
 </html>
--- js/indexJs.js
+++ js/indexJs.js
@@ -1,18 +1,21 @@
 // Выравнивает форму входа по высоте в центре
 var windowHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);
-var divForm = document.getElementById("form");
-var formHeight = Math.max(divForm.offsetHeight, divForm.clientHeight);
-var marginFormTop = (windowHeight - formHeight) / 2;
-divForm.style.marginTop = marginFormTop + "px";
+document.querySelector("main").style.height = windowHeight + "px";
 
 // Меняет инпут с паролем на текстовый инпут и обратно
 function showPass() {
 	var inputPass = document.getElementById("pass");
+	var label = document.querySelector("label:nth-of-type(1)");
+	audio = new Audio();
 	if(inputPass.type == "password") {
 		inputPass.type = "text";
+		label.style.backgroundImage = "url('image/openPassword.jpg')";
+		audio.src = "sound/Isee.wav";
+		audio.autoplay = true;
 	}
 	else {
 		inputPass.type = "password";
+		label.style.backgroundImage = "url('image/closePassword.jpg')";
 	}
 }
 
@@ -21,12 +24,15 @@ function notShowPass() {
 	inputPass.type = "password";
 }
 
-function showNote() {
-	var note = document.querySelector(".note");
-	if(note.style.display == "none") {
-		note.style.display = "inline-block";
+function saveUser(){
+	var label = document.querySelector("label:nth-of-type(2)");
+	audio = new Audio();
+	if(label.style.backgroundImage == 'url("image/saveUser.jpg")'){
+		label.style.backgroundImage = "url('image/noSaveUser.jpg')";
 	}
-	else {
-		note.style.display = "none";
+	else{
+		label.style.backgroundImage = "url('image/saveUser.jpg')";
+		audio.src = "sound/saveUser.wav";
+		audio.autoplay = true;
 	}
 }
